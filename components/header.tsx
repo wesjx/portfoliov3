b2383e6d3d817a3b844f281008f2effd7763de05
Wesley Junio <wesjrpiri@gmail.com>
2026-03-24 20:34:45 +0000

    feat: add verification with path name to show a button if the path name is /freelance.

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,16 @@
 "use client"
 import Image from "next/image";
-import Link from "next/link";
 import { ModeToggle } from "./theme-toggle";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import LoadingIndicator from "./loading-spiner";
 import DrawerMenuMobile from "./drawer-header";
+import { usePathname } from "next/navigation";
+import LanguageToggle from "./freelance-page/freelance-toggle-language";
 
 export default function Header() {
     const { theme, resolvedTheme } = useTheme()
     const [logoHeader, setLogoHeader] = useState('/logo_redondo_preto.png')
-
+    const usePathName = usePathname()
     useEffect(() => {
         if (theme === 'dark' || (theme === 'system' && resolvedTheme === 'dark')) {
             setLogoHeader('/logo_redondo_branco.png')
@@ -24,7 +24,12 @@ export default function Header() {
         <header className="top-0 left-0 p-0 md:p-6 max-w-7xl h-20 flex justify-between items-center mx-auto">
             <Image src={logoHeader} alt="Header Image" width={85} height={85} />
             <DrawerMenuMobile />
-            <ModeToggle />
+            <div className="flex flex-row items-end gap-2">
+                {usePathName === "/freelance" && (
+                    <LanguageToggle />
+                )}
+                <ModeToggle />
+            </div>
         </header>
     )
 }
